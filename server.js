ffe3441690b20ba212efff69ef3e0c84d651dc3d
SUBHA SARKAR <154306366+suryagaming145@users.noreply.github.com>
2026-07-05 18:19:48 +0530

    Change server to use PORT environment variable
    Updated server to use environment variable for port.

--- server.js
+++ server.js
@@ -51,9 +51,11 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
     console.log("=================================");
     console.log("Server Running");
-    console.log("http://localhost:3000");
+    console.log(`Port: ${PORT}`);
     console.log("=================================");
 });
